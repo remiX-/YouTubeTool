dfb4b800c166e5548aaa5c5d813c70d364e8808f
Nicholas Jordaan <njordaan99@gmail.com>
2018-05-03 18:13:25 +0200

    Updated version

---
+++ CHANGELOG.md
@@ -0,0 +1,8 @@
+### v1.0.1 (04 May 2018)
+- Updated libs
+
+### v1.0.0 (03 May 2018)
+
+- First non-alpha release
+- Able to search and view full playlists and single videos
+- Able to download full playlist at once with current download progress
--- YouTubeTool/Properties/AssemblyInfo.cs
+++ YouTubeTool/Properties/AssemblyInfo.cs
@@ -8,7 +8,7 @@ using System.Windows;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("YouTubeTool")]
-[assembly: AssemblyDescription("Just a YouTube GUI Tool for downloading playlists.")]
+[assembly: AssemblyDescription("A YouTube GUI Tool for downloading YouTube playlists & videos as audio and video.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("remiX")]
 [assembly: AssemblyProduct("YouTubeTool")]
@@ -51,5 +51,5 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyVersion("1.0.1")]
+[assembly: AssemblyFileVersion("1.0.1")]
